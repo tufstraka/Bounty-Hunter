c665d1355ab939807f951316bb183c9fcfe5ff59
tufstraka <keithkadima@gmail.com>
2026-01-05 20:20:39 +0300

    feat(ethereum): add transfer function and Transfer event to ERC20 ABI
    Add transfer function signature and Transfer event to enable direct
    token transfers and transaction monitoring for MNEE token interactions.

--- bot/src/services/ethereumPayment.js
+++ bot/src/services/ethereumPayment.js
@@ -19,10 +19,12 @@ const BOUNTY_ESCROW_ABI = [
 // ERC20 ABI for MNEE token interactions
 const ERC20_ABI = [
   "function approve(address spender, uint256 amount) returns (bool)",
+  "function transfer(address to, uint256 amount) returns (bool)",
   "function balanceOf(address account) view returns (uint256)",
   "function allowance(address owner, address spender) view returns (uint256)",
   "function decimals() view returns (uint8)",
-  "function symbol() view returns (string)"
+  "function symbol() view returns (string)",
+  "event Transfer(address indexed from, address indexed to, uint256 value)"
 ];
 
 // MNEE Token address on Ethereum mainnet
